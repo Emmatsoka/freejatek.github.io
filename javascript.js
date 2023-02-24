2012fea8ac54d482db861fb8aa82b72735bcf51c
Emmatsoka <116305770+Emmatsoka@users.noreply.github.com>
2023-02-24 09:28:51 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -55,3 +55,24 @@ window.onload = function() {
     document.body.appendChild(css);
 };
 
+
+
+  const myInput = document.getElementById("gomb");
+
+  // feliratkozunk a focus eseményre
+  myInput.addEventListener("focus", () => {
+    // ha az elemre fókuszálunk, 3 másodperc múlva eltávolítjuk a fókuszt
+    setTimeout(() => {
+      myInput.blur(); // eltávolítja a fókuszt
+    }, 3000); // 3 másodperc (3000 milliszekundum) után hajtja végre a setTimeout() függvényt
+  });
+
+  const myInput = document.getElementById("gorget");
+
+  // feliratkozunk a focus eseményre
+  myInput.addEventListener("focus", () => {
+    // ha az elemre fókuszálunk, 3 másodperc múlva eltávolítjuk a fókuszt
+    setTimeout(() => {
+      myInput.blur(); // eltávolítja a fókuszt
+    }, 3000); // 3 másodperc (3000 milliszekundum) után hajtja végre a setTimeout() függvényt
+  });
